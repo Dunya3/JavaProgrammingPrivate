2d426ea66cab8746a04d7eab276dfe772d06c13c
Dunja <dunjaberic@hotmail.com>
2022-01-14 15:38:20 -0500

    initial commit

--- src/day39_Recap/Cydeo/Person.java
+++ src/day39_Recap/Cydeo/Person.java
@@ -2,11 +2,63 @@ package day39_Recap.Cydeo;
 
 public class Person {
 
-    public String name;
-    public int age;
-    public char gender;
+    private String name;
+    private int age;
+    private char gender;
 
+    public String getName() {
+        return name;
+    }
 
+    public int getAge() {
+        return age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public Person(String name, int age, char gender) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+    }
+
+    public void eat(){
+        System.out.println(name+" is eating");
+
+    }
+
+    public void drink(){
+        System.out.println(name+" is drinking");
+
+    }
+
+    public void sleep(){
+        System.out.println(name+" is sleeping");
+
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
 }
    /* CydeoTask:
             1. Create a class named Person:
