cccd7919fc378a040bc2483308c747024c3d88ff
Dunja <dunjaberic@hotmail.com>
2022-01-27 13:03:01 -0500

    day42

--- src/day42_Exceptions/ThrowKeyWord2.java
+++ src/day42_Exceptions/ThrowKeyWord2.java
@@ -1,13 +1,14 @@
 package day42_Exceptions;
 
+import java.util.NoSuchElementException;
+
 public class ThrowKeyWord2 {
-    public static void main(String[] args) throws ArithmeticException {
+    public static void main(String[] args) {
 
-        System.out.println("Test started");
+         throw new NoSuchElementException("No such Element");
 
-        System.out.println(8/0);
+        // throw new Rectangle(5,7);
 
-        System.out.println("Test completed");
 
 
     }
