fe335453f956cc7f767871cae0fbc7668a5d735a
Dunja <dunjaberic@hotmail.com>
2022-02-07 10:01:18 -0500

    day43

---
+++ src/Day44_Abstraction/AnimalTask/Cat.java
@@ -0,0 +1,17 @@
+package Day44_Abstraction.AnimalTask;
+
+public final class Cat extends Animal{
+
+    public Cat(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName()+" is eating biryani");
+    }
+
+    public void meow(){
+        System.out.println(getName()+"is meowing");
+    }
+}
---
+++ src/day43_Abstraction/Car/Tesla.java
@@ -0,0 +1,18 @@
+package day43_Abstraction.Car;
+
+import day43_Abstraction.Car.Car;
+
+public final class Tesla extends Car {
+
+    public Tesla( String model, String color, int year, double price) {
+        super("Tesla", model, color, year, price);
+    }
+
+    public void start(){
+        System.out.println("Say\"start\"");
+    }
+
+    public void autoPilot(){
+        System.out.println();
+    }
+}
