3d91718480ef42ea7587fe133badb0af3f5d6fb1
Dunja <dunjaberic@hotmail.com>
2022-03-13 11:06:08 -0400

    initial commit

--- src/day15_LoopIntro/FINRA.java
+++ src/day15_LoopIntro/FINRA.java
@@ -3,21 +3,25 @@ package day15_LoopIntro;
 public class FINRA {
     public static void main(String[] args) {
 
-        for(int i = 1; i <= 100; i++){
-
-            if(i % 15 == 0){ // 15, 30, 45, 60, 75, 90... 100
-                System.out.print("FINRA ");
-            }else if(i % 3 ==0){ //3, 6, 9, 12...99
-                System.out.print("FIN ");
-            }else if(i % 5 ==0) {
-                System.out.print("RA ");//5, 10, 15, 20...100
-            }else { //1, 2, 4, 7.....
-                System.out.print(i+" ");
-
-            }
-        }
-
+        Finra(5);
     }
+
+    public static void Finra(int number) {
+        if (number >= 1 && number <= 30) {
+            if (number % 3 == 0 && number%5==0) {
+                System.out.println("FINRA");
+            }else if(number%3 ==0 && number%5!=0){
+                System.out.println("FIN");
+            }else if(number%3!=0 && number%5==0){
+                System.out.println("RA");
+            }else {
+                System.out.println(number+ " is not divisible with 3 or 5");
+            }
+        }else {
+            System.out.println("Please input number between 1 and 30");
+        }
+    }
+
 }
 /*
 
